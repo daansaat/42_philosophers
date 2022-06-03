b9579d56b62dcafbadc09ee983732f4ec455e771
Daniel Saat <dsaat@f1r2s1.codam.nl>
2022-06-03 11:29:29 +0200

    norm

--- philo_bonus/src_bonus/main_bonus.c
+++ philo_bonus/src_bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dsaat <dsaat@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/02 17:23:03 by dsaat         #+#    #+#                 */
-/*   Updated: 2022/06/03 07:59:17 by daansaat      ########   odam.nl         */
+/*   Updated: 2022/06/03 11:29:10 by dsaat         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,8 @@ int	main(int argc, char **argv)
 	init_struct(&data, argv);
 	init_semaphore(&data);
 	init_processes(&data);
-	while (waitpid(0, NULL, 0) > 0);
+	while (waitpid(0, NULL, 0) > 0)
+		;
 	clean_up(&data);
 	return (0);
 }
